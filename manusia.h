ed5ccd4c6ababc5e4bf14ff9f7ae1aa5ee7e7240
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-05-12 19:32:23 +0700

    menambahkan class manusia beserta member  variabel

--- manusia.h
+++ manusia.h
@@ -3,3 +3,18 @@
 #include <iostream>
 using namespace std;
 #include "jantung.h"
+
+class manusia {
+public:
+    string name;
+    jantung varJantung;
+
+    manusia(string pName)
+        : name(pName) {
+        cout << name << " hidup\n";
+    }
+    ~manusia() {
+        cout << name << " mati\n";
+    }
+};
+#endif 
